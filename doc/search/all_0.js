bcc65bcb16e16ecd886b6e7db3bef3866c7dc220
septillion-git <tengelgeer+github@gmail.com>
2016-11-30 14:40:49 +0100

    Gamma correction documentation

--- doc/search/all_0.js
+++ doc/search/all_0.js
@@ -1,9 +1,11 @@
 var searchData=
 [
+  ['_5fbiggeststep',['_biggestStep',['../class_fade_led.html#ab5bfc9d5b2126c768fa299c466c7b0ca',1,'FadeLed']]],
   ['_5fconsttime',['_constTime',['../class_fade_led.html#aaefde460b017166b3eff560c6dff14a0',1,'FadeLed']]],
   ['_5fcount',['_count',['../class_fade_led.html#af75f1a817ff5f45be02b53f7635b43d4',1,'FadeLed']]],
   ['_5fcountmax',['_countMax',['../class_fade_led.html#a8c43b03667e3f3d1f0ae7a360b3f69ce',1,'FadeLed']]],
   ['_5fcurval',['_curVal',['../class_fade_led.html#a5e34d99c7a4aa8bb5462c1bebb547d57',1,'FadeLed']]],
+  ['_5fgammalookup',['_gammaLookup',['../class_fade_led.html#a73ae139d846d381f8edcc9c9a1d63419',1,'FadeLed']]],
   ['_5finterval',['_interval',['../class_fade_led.html#acd7a6fb8e40c3d7e42cd4df6cab34aaf',1,'FadeLed']]],
   ['_5fledcount',['_ledCount',['../class_fade_led.html#a0671cf7bb2fcce089f8c4b65f0c50fc0',1,'FadeLed']]],
   ['_5fledlist',['_ledList',['../class_fade_led.html#adbc0512d73c3b35c63c0ebe2c237d453',1,'FadeLed']]],
--- doc/search/functions_3.js
+++ doc/search/functions_3.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['get',['get',['../class_fade_led.html#a296d8b42151afbca83bc631b32c61939',1,'FadeLed']]],
-  ['getcurrent',['getCurrent',['../class_fade_led.html#a6727918cb455b52cf46e2df697d07a10',1,'FadeLed']]]
+  ['getbiggeststep',['getBiggestStep',['../class_fade_led.html#a393259486c25f0033fa0298683ed88e8',1,'FadeLed']]],
+  ['getcurrent',['getCurrent',['../class_fade_led.html#a6727918cb455b52cf46e2df697d07a10',1,'FadeLed']]],
+  ['getgamma',['getGamma',['../class_fade_led.html#a4b62e0b7b792231df905dad961e5fb0e',1,'FadeLed']]],
+  ['getgammavalue',['getGammaValue',['../class_fade_led.html#ac1d0c1366394ccfcb040c20c87f302a2',1,'FadeLed']]]
 ];
